8c3b47f9026323bba7795e90f2bc4ed62dee35f8
Akshay Ramaswamy <a.ramaswamy1@nuigalway.ie>
2018-04-16 20:38:56 +0100

    web.php and gitignore

--- LaravelMultiSocial/routes/web.php
+++ LaravelMultiSocial/routes/web.php
@@ -44,6 +44,15 @@ Route::post('localadmin/login', 'AdminLoginController@login')->name('admin.login
 Route::get('localadmin/user', 'AdminController@findUser')->name('admin.findUser');
 //localadmin dashboard after login.
 Route::get('localadmin/dashboard', 'AdminController@index')->name('admin.dashboard');
+//localadmin get user data from database
+Route::get('localadmin/getuserlist','AdminController@showuserlist')->name('localadmin.userlist');
+//localadmin make the user admin
+Route::post('localadmin/makeasadmin','AdminController@makeasadmin');
+//localadmin remove the user admin
+Route::post('localadmin/removeadmin','AdminController@removeadmin');
+
+
+
 
 //admin & Staff routes
 //Display available categories
@@ -57,6 +66,9 @@ Route::delete('admin/categories/delete/{id}', 'StaffAdminController@destroyCateg
 
 //Display available categories
 Route::get('admin/products', 'StaffAdminController@showProducts')->name('adminstaff.products');
+//Edit Product for edit
+Route::get('admin/products/edit', 'StaffAdminController@editProduct')->name('adminstaff.products.edit');
+
 //Display activity logs
 Route::get('admin/logs', 'StaffAdminController@showLogs')->name('adminstaff.logs');
 //Export activity logs to excel
@@ -123,13 +135,13 @@ Route::post('/checkout',function(Request $request){
         // Set the receiver and subject of the mail.
         $message->to($admin, "Admin")->subject('New Request in System');
         // Set the sender
-        $message->from('s.halyal1@nuigalway.ie','NUIGsocs Inventory Mail');
+        $message->from('a.ramaswamy1@nuigalway.ie','NUIGsocs Inventory Mail');
         $message->cc($staffanduser);
     });
 
 
 
-   
+
    $Cart = \DB::select('select * from cart WHERE User_Id=?', [$user->id]);
 
    foreach ($Cart as $c) {
@@ -147,3 +159,28 @@ Route::post('/checkout',function(Request $request){
 
 
 // sanjeev end
+
+
+//manmaya's
+Route::get('admin/request','StaffAdminController@pendingdata')->name('adminstaff.request');
+//Route::get('/','CreatesController@pendingdata');
+Route::post('apv/but/{booking_id}', 'CreatesController@approvebutton');
+Route::get('dec/but', function(Request $request)
+{
+ $id=$request->input('booking_id');
+ DB:: table('transactions')->where('booking_id',$id)->update(['booking_status'=>'declined']);
+DB::table ('transactions')->where('booking_id',$id)->update(['reject_comment'=>$request->input('reason')]);
+ //return $id;
+ return back();
+ // echo $request->('ex');
+});//get approved table
+Route::get('admin/request/approved', 'CreatesController@approvedData')->name('adminstaff.request.approved');
+//get declined table
+Route::get('admin/request/declined', 'CreatesController@declinedData')->name('adminstaff.request.declined');
+Route::get('/yo/po', 'CreatesController@exportapproved');//export for approved
+Route::get('/zo/qo', 'CreatesController@exportdeclined');//export for declined//userlist
+Route::get('admin/userlist','StaffAdminController@userlist')->name('adminstaff.userlist');Route::post('mak/stf/{id}', 'ListsController@makestaff');
+Route::post('del/usr/{id}', 'ListsController@deleteuser');
+Route::get('/lo/go', 'ListsController@exportuserlist');Route::get('/userrequest', function () {
+return view('User Homepage.userrequest');
+});
